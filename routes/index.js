af3f83edce6c19ee52ccea5d7ba70ec92bf313be
Yaroslav <yaroslavkoshylko@gmail.com>
2025-06-18 11:58:37 +0300

    feat: post controller

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const multer = require("multer");
-const { UserController } = require("../controllers");
+const { UserController, PostController } = require("../controllers");
 const authentificateToken = require("../middleware/auth");
 
 const uploadDestination = "uploads";
@@ -15,10 +15,17 @@ const storage = multer.diskStorage({
 
 const uploads = multer({ storage: storage });
 
+//USER ROUTES
 router.post("/register", UserController.register);
 router.post("/login", UserController.login);
 router.get("/current", authentificateToken, UserController.current);
 router.get("/users/:id", authentificateToken, UserController.getById);
 router.put("/users/:id", authentificateToken, UserController.update);
+
+//POSTS ROUTES
+router.post("/posts", authentificateToken, PostController.createPost);
+router.get("/posts", authentificateToken, PostController.getAllPosts);
+router.get("/posts/:id", authentificateToken, PostController.getPostById);
+router.delete("/posts/:id", authentificateToken, PostController.deletePost);
 
 module.exports = router;
